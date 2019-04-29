4b5d5412cd04076fc7b45d5aa4de333aba655f84
misztsu <adam.pawlowski.237@gmail.com>
2019-04-29 14:36:17 +0200

    add random particles generation

--- src/symul.cpp
+++ src/symul.cpp
@@ -2,15 +2,17 @@
 #include "symul.h"
 #include <iostream>
 #include <thread>
+#include <random>
 
 const std::vector<particle>& symul::moveParticles(const float& timeTick)
 {
-    particles = afterCollisions.get();
+   	particles = afterCollisions.get();
 
     for(auto& p : particles)
         p.update(timeTick);
 
     afterCollisions = std::async(std::launch::async, collide, particles);
+	
 
     return particles;
 }
@@ -19,18 +21,20 @@ std::vector<particle> symul::collide(std::vector<particle> fparticles)
 {
     // todo collisions 
 
-    // temporary, just to simulate this being the most time consuming part of the whole thing
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-
     return fparticles;
 }
 
 symul::symul(const int& n, const float& r, const vec2f& boxS, const vec2f& iniBox, const float& maxspeed)
     : particleR(r), box(boxS), particles(n) 
 {
-    // todo random particles in proper box
+	std::mt19937 mt_rand(time(0));
+	auto randFloat = [&](float low, float high)
+	{
+		return low + (high - low) * (static_cast<float>(mt_rand()) / static_cast<float>(mt_rand.max()));
+	};
     for(int i = 0; i < n; i++)
-        particles[i] = particle(vec2f(i, i), vec2f(maxspeed, -maxspeed));
+		particles[i] = particle(vec2f(randFloat(0, iniBox.getX()), randFloat(0, iniBox.getY())),
+							    vec2f(randFloat(-maxspeed, maxspeed), randFloat(-maxspeed, maxspeed)));
 
     afterCollisions = std::async(std::launch::async, collide, particles);
 }
