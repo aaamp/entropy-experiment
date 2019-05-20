a8d601dace86a0a52768f47e15fc49e55985646e
misztsu <adam.pawlowski.237@gmail.com>
2019-05-20 23:58:44 +0200

    fix indentation issues

--- src/entropy.h
+++ src/entropy.h
@@ -11,23 +11,23 @@ using namespace std;
 class entropy
 {
 public:
- entropy(float p_grid, float v_grid, int p_count, int v_count, float r, float v);
+    entropy(float p_grid, float v_grid, int p_count, int v_count, float r, float v);
 
- const void ClearBoxes();
- const void LoadParticles(vector<particle>p);
- const void GroupParticles();
- const double& calcProbability();
- const double& calcEntropy();
- const vector<vector< vector<vector<int>>>>& GetBoxes();
+    const void ClearBoxes();
+    const void LoadParticles(vector<particle>p);
+    const void GroupParticles();
+    const double& calcProbability();
+    const double& calcEntropy();
+    const vector<vector< vector<vector<int>>>>& GetBoxes();
 private:
- vector<particle> particles;
- vector<vector< vector<vector<int>>>>boxes;
- float velocity_box_size;
- int velocity_box_count;
- float position_box_size;
- int position_box_count;
- float position_limit;
- float velocity_limit;
+    vector<particle> particles;
+    vector<vector< vector<vector<int>>>>boxes;
+    float velocity_box_size;
+    int velocity_box_count;
+    float position_box_size;
+    int position_box_count;
+    float position_limit;
+    float velocity_limit;
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -14,42 +14,42 @@ int main()
     display.createWindow("Symulacja", 1000, 500);
     display.zoom(0.2);
     display.setCenter(100,50);
- 
+
     symul symulation = symul(50000, 0.2, vec2f(200, 100), vec2f(100, 100), 0.3, 0.1);
     std::vector<particle> particicles = symulation.getParticles();
 
-	entropy ent(2, 2, 5, 4, 5, 4);
+    entropy ent(2, 2, 5, 4, 5, 4);
 
     auto lastTime = std::chrono::high_resolution_clock::now();
     for(int tick = 0; tick < 100000; tick++)
     {
         std::cout << "tick " << tick << std::endl;
         /*
-       std::cout << "tick " << tick << std::endl;
-       for(auto i : symulation.getParticles())
+           std::cout << "tick " << tick << std::endl;
+           for(auto i : symulation.getParticles())
            std::cout << i.getPosition() << std::endl;
-       std::cout << std::endl;
-       */ 
+           std::cout << std::endl;
+           */ 
 
         /*if(tick > 0)  
-            std::this_thread::sleep_until(lastTime + std::chrono::milliseconds(100));
-        lastTime = std::chrono::high_resolution_clock::now();*/
+          std::this_thread::sleep_until(lastTime + std::chrono::milliseconds(100));
+          lastTime = std::chrono::high_resolution_clock::now();*/
 
         // To czy szybsza jest fizyka czy grafika zależy aktualnie od powiększenia obrazu,
         // fizyka nie powoduje lagów.
 
         particicles = symulation.getParticles();
-  ent.LoadParticles(particicles);
-  ent.GroupParticles();
-  double entropy = ent.calcEntropy();
-  double probability = pow(exp(1), entropy); //nie wiem, czy jest Wam to do szczęścia potrzebne, feel free to comment
-  vector<vector< vector<vector<int>>>>a = ent.GetBoxes();
-  ent.ClearBoxes();
+        ent.LoadParticles(particicles);
+        ent.GroupParticles();
+        double entropy = ent.calcEntropy();
+        double probability = pow(exp(1), entropy); //nie wiem, czy jest Wam to do szczęścia potrzebne, feel free to comment
+        vector<vector< vector<vector<int>>>>a = ent.GetBoxes();
+        ent.ClearBoxes();
 
         std::future<void> task = std::async(std::launch::async, [&symulation]() {
-            symulation.moveParticles();
-        });
-        
+                symulation.moveParticles();
+                });
+
         display.drawParticles(particicles);
         display.pollEvents();
         while (task.wait_for(std::chrono::milliseconds(20)) != std::future_status::ready)
