9e530a66a7cebc4ba3ad78e684a2adf0e0d453b5
misztsu <adam.pawlowski.237@gmail.com>
2019-05-21 21:23:35 +0200

    refactor main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,34 @@
 
 int main()
 {
-    displ display(0.2, vec2f(200, 100));
+    // number of particles
+    const int PAR_N = 50000;
+
+    // box size
+    // upper left corner of the box is at (0, 0)
+    // bottom right at BOX_XY
+    const vec2f BOX_XY(200, 100);
+
+    // box in which particles start
+    // from (0, 0) to BOX_S
+    const vec2f BOX_S(100, 100);
+
+    // particle radius
+    const float RAD = 0.2;
+
+    // speed of a particle at the start is chosen randomly from
+    // { (x, y) : x, y \in [-SPEED, SPEED] }
+    const float SPEED = 0.3;
+
+
+
+    displ display(RAD, BOX_XY);
     display.createWindow("Symulacja", 1000, 500);
     display.zoom(0.2);
     display.setCenter(100,50);
 
-    symul symulation = symul(50000, 0.2, vec2f(200, 100), vec2f(100, 100), 0.3, 0.1);
-    std::vector<particle> particicles = symulation.getParticles();
+    symul symulation = symul(PAR_N, RAD, BOX_XY, BOX_S, SPEED);
+    std::vector<particle> particles = symulation.getParticles();
 
     entropy ent(2, 2, 5, 4, 5, 4);
 
@@ -24,12 +45,6 @@ int main()
     for(int tick = 0; tick < 100000; tick++)
     {
         std::cout << "tick " << tick << std::endl;
-        /*
-           std::cout << "tick " << tick << std::endl;
-           for(auto i : symulation.getParticles())
-           std::cout << i.getPosition() << std::endl;
-           std::cout << std::endl;
-           */ 
 
         /*if(tick > 0)  
           std::this_thread::sleep_until(lastTime + std::chrono::milliseconds(100));
@@ -38,25 +53,32 @@ int main()
         // To czy szybsza jest fizyka czy grafika zależy aktualnie od powiększenia obrazu,
         // fizyka nie powoduje lagów.
 
-        particicles = symulation.getParticles();
-        ent.LoadParticles(particicles);
-        ent.GroupParticles();
-        double entropy = ent.calcEntropy();
-        double probability = pow(exp(1), entropy); //nie wiem, czy jest Wam to do szczęścia potrzebne, feel free to comment
-        vector<vector< vector<vector<int>>>>a = ent.GetBoxes();
-        ent.ClearBoxes();
+        particles = symulation.getParticles();
 
-        std::future<void> task = std::async(std::launch::async, [&symulation]() {
-                symulation.moveParticles();
-                });
+        std::future<void> symulTask = std::async(std::launch::async, [&symulation]() {
+            symulation.moveParticles();
+        });
 
-        display.drawParticles(particicles);
+        std::future<double> entropyTask = std::async(std::launch::async, [&ent, &particles]() {
+            ent.ClearBoxes();
+            ent.LoadParticles(particles);
+            ent.GroupParticles();
+            return ent.calcEntropy();
+            //double probability = pow(exp(1), entropy); //nie wiem, czy jest Wam to do szczęścia potrzebne, feel free to comment
+        });
+
+        display.drawParticles(particles);
         display.pollEvents();
-        while (task.wait_for(std::chrono::milliseconds(20)) != std::future_status::ready)
+
+        while (symulTask.wait_for(std::chrono::milliseconds(20)) != std::future_status::ready
+            || entropyTask.wait_for(std::chrono::milliseconds(20)) != std::future_status::ready)
         {
-            display.drawParticles(particicles);
+            display.drawParticles(particles);
             display.pollEvents();
         }
+
+        double e = entropyTask.get();
+        cout << "Entropy: " << e << endl;
     }
 }
 
